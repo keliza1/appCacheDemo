bc10a9da09d707a9cfe9a94176ce1db2ec2d8293
keliza1 <keliza1@tigers.lsu.edu>
2016-05-20 12:55:45 -0500

    Add files via upload

---
+++ isAliveController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppCacheDemo_1.Controllers
+{
+    public class T_t
+    {
+        public Boolean v;
+    }
+    public class isAliveController : ApiController
+    {
+        public T_t Get()
+        {
+            var v = new T_t();
+            v.v = true;
+            return v;
+        }
+    }
+
+    
+}
