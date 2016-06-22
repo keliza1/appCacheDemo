d310891ab0710e70482cb3d187c4a4ed4524d2de
keliza1 <keliza1@tigers.lsu.edu>
2016-06-22 16:53:17 -0500

    Update isAliveController.cs

--- isAliveController.cs
+++ isAliveController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -20,8 +20,14 @@ namespace AppCacheDemo_1.Controllers
             v.v = true;
             return v;
         }
-        
-        //need random number generator to randomize true/false response
+
+        /*need random number generator to randomize true/false response
+         * generate random integer from 1-10
+         * for (i<6) { v = true }
+         * else { v = false }
+         * can change percentage of time v is equal to true and false
+        */
+
     }
 
     
